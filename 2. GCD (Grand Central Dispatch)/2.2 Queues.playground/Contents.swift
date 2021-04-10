db96c729955c84459c0acac035523132e516a565
Egor Gorskikh <60622982+egorskikh@users.noreply.github.com>
2021-04-10 13:58:52 +0300

    update

---
+++ 2. GCD (Grand Central Dispatch)/2.2 Queues.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class QueueTest1 {
+    
+    private let serialQueue = DispatchQueue(label: "serialTest")
+    
+    private let concurrentQueue = DispatchQueue(label: "concurrentTest", attributes: .concurrent)
+    
+}
+
+class QueueTest2 {
+    private let globalQueue = DispatchQueue.global()
+    private let mainQueue = DispatchQueue.main
+}
